c856cb1fb68a8aeeaad593edc427a088397ed130
medicare-github <mcareducation@gmail.com>
2022-04-15 01:24:57 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,353 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- The above 4 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+
+    <!-- Title -->
+    <!-- Title -->
+    <title>iTcare | About</title>
+
+    <!-- Favicon -->
+    <link rel="icon" href="img/favicon.png">
+
+    <!-- Core Stylesheet -->
+    <link href="style.css" rel="stylesheet">
+
+    <!-- Responsive CSS -->
+    <link href="css/responsive.css" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/3c13386cd6.js" crossorigin="anonymous"></script>
+
+</head>
+
+<body>
+    <!-- ***** Preloader Start ***** -->
+    <div id="preloader">
+        <div class="mosh-preloader"></div>
+    </div>
+
+    <!-- ***** Header Area Start ***** -->
+    <header class="header_area clearfix">
+            <div class="container-fluid h-100">
+                <div class="row h-100">
+                    <!-- Menu Area Start -->
+                    <div class="col-12 h-100">
+                        <div class="menu_area h-100">
+                            <nav class="navbar h-100 navbar-expand-lg align-items-center">
+                              <a class="navbar-brand" href="index.html"><img src="img/logo1.png" alt="logo" width="90"></a>
+                                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#mosh-navbar" aria-controls="mosh-navbar" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+
+                                <div class="collapse navbar-collapse justify-content-end" id="mosh-navbar">
+                                    <ul class="navbar-nav animated" id="nav">
+                                        <li class="nav-item"><a class="nav-link" href="about.html">about Me</a></li>
+                                    </ul>
+                                    <!-- Search Form Area Start -->
+                                    <div class="search-form-area animated">
+                                        <form action="#" method="post">
+                                            <input type="search" name="search" id="search" placeholder="Type keywords &amp; hit enter">
+                                            <button type="submit" class="d-none"><img src="img/core-img/search-icon.png" alt="Search"></button>
+                                        </form>
+                                    </div>
+                                    <!-- Search btn -->
+                                    <div class="search-button">
+                                        <a href="#" id="search-btn"><img src="img/core-img/search-icon.png" alt="Search"></a>
+                                    </div>
+                                    <!-- Login/Register btn -->
+
+                                </div>
+                            </nav>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    </header>
+    <!-- ***** Header Area End ***** -->
+
+    <!-- ***** Breadcumb Area Start ***** -->
+    <div class="mosh-breadcumb-area" style="background-image: url(img/core-img/breadcumb.png);">
+        <div class="container h-100">
+            <div class="row h-100 align-items-center">
+                <div class="col-12">
+                    <div class="bradcumbContent">
+                        <h2>Data Diri Medicare</h2>
+                        <nav aria-label="breadcrumb">
+                            <ol class="breadcrumb">
+                                <li class="breadcrumb-item"><a href="#">Home</a></li>
+                            </ol>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- ***** Breadcumb Area End ***** -->
+
+    <!-- ***** About Us Area Start ***** -->
+    <section class="mosh-aboutUs-area section_padding_100_0">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-12 col-md-8">
+                    <div class="mosh-about-us-content">
+                        <div class="section-heading">
+                            <h2>Biodata Medicare</h2>
+                        </div>
+                        <p>saya adalah wisudawan 2020 <a href="https://universitasbumigora.ac.id/" target="_blank">Universitas Bumigora</a> Mataram yang menjadi junior programmer menggunakan Framework <a href="https://flutter.dev/" target="blank">Flutter </a> untuk membangun dan mengembangkan mobile app sistem operasi <a href="https://developer.android.com/about/versions/11" target="blank">Android</a>  dan <a href="https://www.apple.com/id/ios/ios-15/" target="blank"> iOS </a> , dan juga menggunakan Framework <a href="https://laravel.com/" target="blank"> Laravel </a> , <a href="https://reactjs.org/" target="blank">React js</a> untuk pembangunan Website. 
+                            
+                    </div>
+                </div>
+                <div class="col-12 col-md-4">
+                    <div class="mosh-about-us-thumb wow fadeInUp" data-wow-delay="0.5s">
+                        <img src="img/medicare.JPG" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- ***** About Us Area End ***** -->
+    <section class="mosh-features-area section_padding_100 clearfix">
+        <div class="container">
+            <div class="row left-content-end">
+                <div class="col-8 col-md-8">
+                    <div class="section-heading">
+                        <p>SKILL</p>
+                        <h2>Keahlian di bidang Pemrograman</h2>
+                    </div>
+                    <div class="features-content">
+
+                        <div class="features-progress-bar mt-50">
+                            <!-- Single Progress Bar -->
+                            <div class="single_progress_bar mb-15 wow fadeInUp" data-wow-delay="0.2s">
+                                <p>HTML</p>
+                                <div id="bar1" class="barfiller">
+                                    <div class="tipWrap">
+                                        <span class="tip"></span>
+                                    </div>
+                                    <span class="fill" data-percentage="80"></span>
+                                </div>
+                            </div>
+                            <div class="single_progress_bar mb-15 wow fadeInUp" data-wow-delay="0.2s">
+                                <p>Flutter</p>
+                                <div id="bar2" class="barfiller">
+                                    <div class="tipWrap">
+                                        <span class="tip"></span>
+                                    </div>
+                                    <span class="fill" data-percentage="70"></span>
+                                </div>
+                            </div>
+                            <div class="single_progress_bar mb-15 wow fadeInUp" data-wow-delay="0.2s">
+                                <p>Laravel</p>
+                                <div id="bar3" class="barfiller">
+                                    <div class="tipWrap">
+                                        <span class="tip"></span>
+                                    </div>
+                                    <span class="fill" data-percentage="65"></span>
+                                </div>
+                            </div>
+                            <div class="single_progress_bar mb-15 wow fadeInUp" data-wow-delay="0.2s">
+                                <p>React js</p>
+                                <div id="bar4" class="barfiller">
+                                    <div class="tipWrap">
+                                        <span class="tip"></span>
+                                    </div>
+                                    <span class="fill" data-percentage="65"></span>
+                                </div>
+                            </div>
+                            <div class="single_progress_bar mb-15 wow fadeInUp" data-wow-delay="0.2s">
+                                <p>Java Script</p>
+                                <div id="bar5" class="barfiller">
+                                    <div class="tipWrap">
+                                        <span class="tip"></span>
+                                    </div>
+                                    <span class="fill" data-percentage="65"></span>
+                                </div>
+                            </div>
+                           
+                            <div class="single_progress_bar mb-15 wow fadeInUp" data-wow-delay="0.2s">
+                                <p>mysql</p>
+                                <div id="bar7" class="barfiller">
+                                    <div class="tipWrap">
+                                        <span class="tip"></span>
+                                    </div>
+                                    <span class="fill" data-percentage="85"></span>
+                                </div>
+                            </div>
+                            <div class="single_progress_bar mb-15 wow fadeInUp" data-wow-delay="0.2s">
+                                <p>firebase</p>
+                                <div id="bar8" class="barfiller">
+                                    <div class="tipWrap">
+                                        <span class="tip"></span>
+                                    </div>
+                                    <span class="fill" data-percentage="70"></span>
+                                </div>
+                            </div>
+                            <!-- Single Progress Bar -->
+                            <div class="single_progress_bar mb-15 wow fadeInUp" data-wow-delay="0.4s">
+                                <p>Design UI</p>
+                                <div id="bar6" class="barfiller">
+                                    <div class="tipWrap">
+                                        <span class="tip"></span>
+                                    </div>
+                                    <span class="fill" data-percentage="80"></span>
+                                </div>
+                            </div>
+                            <!-- Single Progress Bar -->
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="mosh-portfolio-area section_padding_100">
+        <div class="container">
+            <div class="section-heading">
+                <h2>Forto Polio Medicare</h2>
+            </div>
+            <div class="row">
+                
+                <div class="col-6">
+                    <div class="mosh-projects-menu">
+                        <div class="portfolio-menu">
+                            <p class="active" data-filter="*">All</p>
+                            <p data-filter=".mb">Mobile</p>
+                            <p data-filter=".wb">Website</p>
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="mosh-portfolio">
+            <!-- Single gallery Item Start -->
+            <div class="single_gallery_item mb">
+                <img src="img/portfolio-img/mobileKlu.png" alt="">
+                <div class="gallery-hover-overlay d-flex align-items-center justify-content-center">
+                    <div class="port-hover-text text-center">
+                        <a href="newKlu.php" target="blank">
+                            <h4>Aplikasi Wisata KLU</h4>
+                        </a>
+                        <h6>by Medicare</h6>
+                    </div>
+                </div>
+            </div>
+            <!-- Single gallery Item Start -->
+            <div class="single_gallery_item wb">
+                <img src="img/portfolio-img/webMobileKlu.png" alt="">
+                <div class="gallery-hover-overlay d-flex align-items-center justify-content-center">
+                    <div class="port-hover-text text-center">
+                        <a href="webNewKLU.php" target="blank">
+                            <h4>Web Manajemen data Wisata KLU</h4>
+                        </a>
+                        <h6>by Medicare</h6>
+                    </div>
+                </div>
+            </div>
+            <!-- Single gallery Item Start -->
+            <div class="single_gallery_item mb">
+                <img src="img/portfolio-img/HaversineLobar.png" alt="">
+                <div class="gallery-hover-overlay d-flex align-items-center justify-content-center">
+                    <div class="port-hover-text text-center">
+                        <a href="haversineLobarWisata.php" target="blank">
+                            <h4>Aplikasi Wisata Lombok Barat dengan Metode Haversine</h4>
+                        </a>
+                        <h6>by Medicare</h6>
+                    </div>
+                </div>
+            </div>
+            <!-- Single gallery Item Start -->
+            <div class="single_gallery_item wb">
+                <img src="img/portfolio-img/metodeKnn.png" alt="">
+                <div class="gallery-hover-overlay d-flex align-items-center justify-content-center">
+                    <div class="port-hover-text text-center">
+                        <a href="websiteKnn.php" target="blank">
+                            <h4>Website Rekomendasi Jurusan Universitas Bumigora dgn Metode KNN</h4>
+                        </a>
+                        <h6>by Medicare</h6>
+                    </div>
+                </div>
+            </div>
+            
+        </div>
+    </section>
+
+        
+        <!-- Feature Thumb -->
+
+    <!-- ***** Footer Area Start ***** -->
+    <footer class="footer-area clearfix">
+        <!-- Top Fotter Area -->
+        <div class="top-footer-area section_padding_100_0">
+             <div class="container">
+                <div class="row">
+                    <div class="col-12 col-sm-6 col-lg-3">
+                        <div class="single-footer-widget mb-100">
+                            <h2 style="color: white;">Contact Info</h2> <br>
+                            <div class="footer-single-contact-info d-flex">
+                                <div class="contact-icon">
+                                    <img src="img/core-img/map.png" alt="">
+                                </div>
+                                <p>Jalan Pariwisata Senaru <br> Lokok Kelungkung, Desa Senaru, <br> Kec. Bayan, KLU, 83354</p>
+                            </div>
+                            <div class="footer-single-contact-info d-flex">
+                                <div class="contact-icon">
+                                    <img src="img/core-img/call.png" alt="">
+                                </div>
+                                <p>Wa/celular: +62878-5932-1747 
+                            </div>
+                            <div class="footer-single-contact-info d-flex">
+                                <div class="contact-icon">
+                                    <img src="img/core-img/message.png" alt="">
+                                </div>
+                                <p>mcareducation@gmail.com</p>
+                            </div>
+                        </div>
+                    </div> -->
+                </div>
+            </div> -->
+        </div>
+        <!-- Fotter Bottom Area -->
+        <div class="footer-bottom-area">
+            <div class="container h-100">
+                <div class="row h-100">
+                    <div class="col-12 h-100">
+                        <div class="footer-bottom-content h-100 d-md-flex justify-content-between align-items-center">
+                            <div class="copyright-text">
+                                <p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+Copyright &copy;<script>document.write(new Date().getFullYear());</script> berbagi di dunia iT <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://m4care.wordpress.com" target="_blank">medicare</a>
+<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+</p>
+                            </div>
+                            <div class="footer-social-info">
+                                <a href="#"><i class="far fa-envelope" aria-hidden="true"></i></a>
+                                <a href="#"><i class="fab fa-facebook" aria-hidden="true"></i></a>
+                                <a href="#"><i class="fab fa-twitter" aria-hidden="true"></i></a>
+                                <a href="#"><i class="fab fa-instagram" aria-hidden="true"></i></a>
+                                <a href="#"><i class="fab fa-whatsapp" aria-hidden="true"></i></a>
+                                <a href="#"><i class="fab fa-linkedin" aria-hidden="true"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!-- ***** Footer Area End ***** -->
+
+    <!-- jQuery-2.2.4 js -->
+    <script src="js/jquery-2.2.4.min.js"></script>
+    <!-- Popper js -->
+    <script src="js/popper.min.js"></script>
+    <!-- Bootstrap js -->
+    <script src="js/bootstrap.min.js"></script>
+    <!-- All Plugins js -->
+    <script src="js/plugins.js"></script>
+    <!-- Active js -->
+    <script src="js/active.js"></script>
+</body>
+
+</html>
---
+++ websiteKnn.php
@@ -0,0 +1,126 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- The above 4 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+
+    <!-- Title -->
+    <!-- Title -->
+    <title>iTcare | porto folio</title>
+
+    <!-- Favicon -->
+    <link rel="icon" href="img/favicon.png">
+
+    <!-- Core Stylesheet -->
+    <link href="style.css" rel="stylesheet">
+
+    <!-- Responsive CSS -->
+    <link href="css/responsive.css" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/3c13386cd6.js" crossorigin="anonymous"></script>
+
+</head>
+
+<body>
+    <!-- ***** Preloader Start ***** -->
+    <div id="preloader">
+        <div class="mosh-preloader"></div>
+    </div>
+
+    <!-- ***** Header Area Start ***** -->
+    <header class="header_area clearfix">
+            <div class="container-fluid h-100">
+                <div class="row h-100">
+                    <!-- Menu Area Start -->
+                    <div class="col-12 h-100">
+                        <div class="menu_area h-100">
+                            <nav class="navbar h-100 navbar-expand-lg align-items-center">
+                              <a class="navbar-brand" href="index.html"><img src="img/logo1.png" alt="logo" width="90"></a>
+                                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#mosh-navbar" aria-controls="mosh-navbar" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+
+                                <div class="collapse navbar-collapse justify-content-end" id="mosh-navbar">
+                                    <ul class="navbar-nav animated" id="nav">
+                                        <li class="nav-item active"><a class="nav-link" href="about.html">about Me</a></li>
+                                    </ul>
+                                    <!-- Search Form Area Start -->
+                                    <div class="search-form-area animated">
+                                        <form action="#" method="post">
+                                            <input type="search" name="search" id="search" placeholder="Type keywords &amp; hit enter">
+                                            <button type="submit" class="d-none"><img src="img/core-img/search-icon.png" alt="Search"></button>
+                                        </form>
+                                    </div>
+                                    <!-- Search btn -->
+                                    <div class="search-button">
+                                        <a href="#" id="search-btn"><img src="img/core-img/search-icon.png" alt="Search"></a>
+                                    </div>
+                                    <!-- Login/Register btn -->
+
+                                </div>
+                            </nav>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    </header>
+    <!-- ***** Header Area End ***** -->
+
+    <!-- ***** Breadcumb Area Start ***** -->
+    <div class="mosh-breadcumb-area" style="background-image: url(img/core-img/breadcumb.png);">
+        <div class="container h-100">
+            <div class="row h-100 align-items-center">
+                <div class="col-12">
+                    <div class="bradcumbContent">
+                        <h2>Website Rekomendasi Jurusan UBG dgn Metode KNN</h2>
+                        <nav aria-label="breadcrumb">
+                            <ol class="breadcrumb">
+                                <li class="breadcrumb-item"><a href="#">Home</a></li>
+                            </ol>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- ***** Breadcumb Area End ***** -->
+    <section class="few-words-from-ceo d-md-flex">
+        <div class="few-words-contents d-flex align-items-center justify-content-center wow fadeInLeftBig" data-wow-delay="0.1s" style="visibility: visible; animation-delay: 0.1s; animation-name: fadeInLeftBig;">
+            <div class="container-fluid">
+                <div class="row justify-content-center">
+                    <div class="col-12 col-md-11 col-lg-9 col-xl-7">
+                        <div class="few-words-text">
+                            <div class="section-heading">
+                                <p>Forto Polio</p>
+                                <h2>Penjelasan Tentang Website Rekomendasi Jurusan UBG dgn Metode KNN</h2>
+                            </div>
+                            <p>Website adalah website yg di bangun untuk menyelesaikan tugas Mata Kuliah Data Mining, dengan memanfaatkan metode K-Nearest Neighbor dapat memberikan rekomendasi jurusan bagi mahasiswa baru berdasarkan Nilai Ujian Nasional. </p>
+                            <div class="ceo-meta-data d-flex align-items-center mt-50">
+                                    <div class="row">
+                                        <div class="col-md-6">
+                                            <a href="https://github.com/medicare-github/Data-Mining" target="blank" class="btn mosh-btn">View Source Code</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="few-words-thumb bg-img wow fadeInRightBig" data-wow-delay="1.1s" style="background-image: url(&quot;img/portfolio-img/metodeKnn.png&quot;); visibility: visible; animation-delay: 1.1s; animation-name: fadeInRightBig;"></div>
+    </section>
+    <!-- jQuery-2.2.4 js -->
+    <script src="js/jquery-2.2.4.min.js"></script>
+    <!-- Popper js -->
+    <script src="js/popper.min.js"></script>
+    <!-- Bootstrap js -->
+    <script src="js/bootstrap.min.js"></script>
+    <!-- All Plugins js -->
+    <script src="js/plugins.js"></script>
+    <!-- Active js -->
+    <script src="js/active.js"></script>
+</body>
+
+</html>
